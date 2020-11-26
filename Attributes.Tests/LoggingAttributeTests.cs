1adb3ea8349821c92a95c85c786b2919320e8db0
Harald Wilbertz <harald.wilbertz@gmx.de>
2020-11-26 20:13:09 +0100

    Additional unit tests.

--- Attributes.Tests/LoggingAttributeTests.cs
+++ Attributes.Tests/LoggingAttributeTests.cs
@@ -1,15 +1,80 @@
 using System;
 using System.Reflection;
 using System.Threading.Tasks;
+
 using FluentAssertions;
 using Moq;
 using NLog;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace AOP.Attributes.Tests
 {
-    public class LoggingAttributeTests
+    public class LoggingAttributeTestsSetupCode : IDisposable
     {
+        public ITestOutputHelper Output { get; set; }
+
+        public Mock<ILogger> MockedLogger { get; set; }
+
+        public LoggingAttributeTestsSetupCode()
+        {
+            // setup code
+            MockedLogger = GetMockedLogger();
+
+            typeof(LoggingAttribute)
+                .GetField("Logger", BindingFlags.Static | BindingFlags.NonPublic)
+                ?.SetValue(null, MockedLogger.Object);
+        }
+
+        public void Dispose()
+        {
+            // clean-up code
+        }
+
+        private Mock<ILogger> GetMockedLogger()
+        {
+            var mockedLogger = new Mock<ILogger>();
+
+            mockedLogger.Setup(m => m.Fatal(It.IsAny<string>())).Callback<string>(WriteLine);
+            mockedLogger.Setup(m => m.Error(It.IsAny<string>())).Callback<string>(WriteLine);
+            mockedLogger.Setup(m => m.Warn(It.IsAny<string>())).Callback<string>(WriteLine);
+            mockedLogger.Setup(m => m.Info(It.IsAny<string>())).Callback<string>(WriteLine);
+            mockedLogger.Setup(m => m.Debug(It.IsAny<string>())).Callback<string>(WriteLine);
+            mockedLogger.Setup(m => m.Trace(It.IsAny<string>())).Callback<string>(WriteLine);
+
+            mockedLogger.SetupGet(p => p.IsFatalEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsErrorEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsWarnEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsInfoEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsDebugEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsTraceEnabled).Returns(true);
+
+            return mockedLogger;
+        }
+
+        private void WriteLine(string s)
+        {
+            Output?.WriteLine(s);
+        }
+    }
+
+    public class LoggingAttributeTests : IClassFixture<LoggingAttributeTestsSetupCode>
+    {
+        private readonly Mock<ILogger> _mockedLogger;
+        private readonly ClassUnderTest _classUnderTest;
+        private readonly ITestOutputHelper _output;
+
+        private Logger Logger { get; } = LogManager.GetCurrentClassLogger();
+
+        public LoggingAttributeTests(LoggingAttributeTestsSetupCode fixture, ITestOutputHelper output)
+        {
+            fixture.Output = output;
+            _mockedLogger = fixture.MockedLogger;
+            _output = output;
+            _mockedLogger.Invocations.Clear();
+            _classUnderTest = new ClassUnderTest();
+        }
+
         #region Tests ---------------------------------------------------------
 
         [Fact]
@@ -23,6 +88,45 @@ namespace AOP.Attributes.Tests
             // Assert
             instance.Should().NotBeNull();
             instance.Should().BeOfType<LoggingAttribute>();
+        }
+
+        [Fact]
+        public void Method_Entry_And_Exit_Should_Be_Logged()
+        {
+            // Arrange 
+            // Act
+            _classUnderTest.MethodToBeTested();
+
+            _output.WriteLine("Harald");
+            // Assert
+            _mockedLogger.Verify(m => m.Fatal(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Error(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Warn(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Info(It.Is<string>(s =>
+                s.Equals("Init: AOP.Attributes.Tests.LoggingAttributeTests+ClassUnderTest.MethodToBeTested [0] params"))), Times.Once);
+            _mockedLogger.Verify(m => m.Info(It.Is<string>(s => s.Equals("Exit: []"))), Times.Once);
+            _mockedLogger.Verify(m => m.Debug(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Trace(It.IsAny<string>()), Times.Never);
+        }
+
+        [Fact]
+        public void Method_Arguments_Should_Be_Logged()
+        {
+            // Arrange 
+            // Act
+            _classUnderTest.MethodWith2ArgumentsToBeTested(42, "UnitTest");
+
+            // Assert
+            _mockedLogger.Verify(m => m.Fatal(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Error(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Warn(It.IsAny<string>()), Times.Never);
+            _mockedLogger.Verify(m => m.Info(It.Is<string>(s =>
+                s.Equals("Init: AOP.Attributes.Tests.LoggingAttributeTests+ClassUnderTest.MethodWith2ArgumentsToBeTested [2] params"))), Times.Once);
+            _mockedLogger.Verify(m => m.Debug(It.IsAny<string>()), Times.Exactly(2));
+            _mockedLogger.Verify(m => m.Debug(It.Is<string>(s => s.Equals("firstArgument: 42"))), Times.Once);
+            _mockedLogger.Verify(m => m.Debug(It.Is<string>(s => s.Equals("secondArgument: UnitTest"))), Times.Once);
+            _mockedLogger.Verify(m => m.Info(It.Is<string>(s => s.Equals("Exit: []"))), Times.Once);
+            _mockedLogger.Verify(m => m.Trace(It.IsAny<string>()), Times.Never);
         }
 
         #endregion
@@ -81,13 +185,6 @@ namespace AOP.Attributes.Tests
             }
         }
 
-        private void SetLoggingInterfaceInAttributeOfClassUnderTest(ILogger logger)
-        {
-            typeof(LoggingAttribute)
-                .GetField("Logger", BindingFlags.Static | BindingFlags.NonPublic)
-                ?.SetValue(null, logger);
-        }
-
         private Mock<ILogger> GetMockedLogger()
         {
             var mockedLogger = new Mock<ILogger>();
@@ -98,6 +195,13 @@ namespace AOP.Attributes.Tests
             mockedLogger.Setup(m => m.Info(It.IsAny<string>())).Callback<string>(Console.WriteLine);
             mockedLogger.Setup(m => m.Debug(It.IsAny<string>())).Callback<string>(Console.WriteLine);
             mockedLogger.Setup(m => m.Trace(It.IsAny<string>())).Callback<string>(Console.WriteLine);
+
+            mockedLogger.SetupGet(p => p.IsFatalEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsErrorEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsWarnEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsInfoEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsDebugEnabled).Returns(true);
+            mockedLogger.SetupGet(p => p.IsTraceEnabled).Returns(true);
 
             return mockedLogger;
         }
--- Attributes/LoggingAttribute.cs
+++ Attributes/LoggingAttribute.cs
@@ -1,8 +1,70 @@
-﻿using MethodBoundaryAspect.Fody.Attributes;
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MethodBoundaryAspect.Fody.Attributes;
+using NLog;
 
 namespace AOP.Attributes
 {
     public sealed class LoggingAttribute : OnMethodBoundaryAspect
     {
+        private static int _firstIndentation = -1;
+
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            if (!(args.Method.DeclaringType is null))
+            {
+                var blubber =
+                    $"{GetIndentation()}Init: {args.Method.DeclaringType.FullName}.{args.Method.Name} [{args.Arguments.Length}] params";
+                Logger.Info($"{GetIndentation()}Init: {args.Method.DeclaringType.FullName}.{args.Method.Name} [{args.Arguments.Length}] params");
+            }
+            foreach (var item in args.Method.GetParameters())
+            {
+                Logger.Debug($"{GetIndentation()}{item.Name}: {args.Arguments[item.Position]}");
+            }
+        }
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            if (args.ReturnValue is Task t)
+            {
+                t.ContinueWith(task =>
+                {
+                    if (task.GetType().IsGenericType)
+                    {
+                        dynamic taskWithResult = task;
+                        Logger.Info($"{GetIndentation(1)}Exit: [{taskWithResult.Result}]");
+                    }
+                    else
+                    {
+                        Logger.Info($"{GetIndentation(1)}Exit: []");
+                    }
+                });
+            }
+            else
+            {
+                Logger.Info($"{GetIndentation()}Exit: [{args.ReturnValue}]");
+            }
+        }
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            Logger.Error($"{GetIndentation()}OnException: {args.Exception.GetType()}: {args.Exception.Message}");
+        }
+
+        private string GetIndentation(int additionalIndentation = 0)
+        {
+            StackTrace st = new StackTrace(false);
+            if (_firstIndentation == -1)
+            {
+                _firstIndentation = st.FrameCount;
+            }
+
+            return String.Concat(Enumerable.Repeat("   ", Math.Max(st.FrameCount + additionalIndentation - _firstIndentation, 0)));
+        }
     }
 }
